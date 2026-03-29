f90c03dd9fd715906abb7839d78485eed2e13e7d
roba7o <rhm14148@gmail.com>
2026-03-29 10:54:16 +0100

    still to complete with json dumps

--- phase_0/HTTPserver.py
+++ phase_0/HTTPserver.py
@@ -4,6 +4,10 @@ from pathlib import Path
 local_path = Path(__file__).parent
 addr = "localhost", 9000
 
+list_var_module_level = []
+
+# TODO -> handle requests better with json.dumps and jsonloads
+
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -24,7 +28,25 @@ class MyHandler(BaseHTTPRequestHandler):
             self.send_header("Content-Type", "text/html")
             self.end_headers()
             with open(local_path / "phase_0.html", "r") as file:
-                self.wfile.write(file.read().encode())
+                self.wfile.write(file.read(5096).encode())
+
+        elif self.path.lower() == "/bookmark":
+            print("MyServer: I got a GET request")
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(list_var_module_level.encode())
+
+    def do_POST(self):
+        if self.path.lower() == "/bookmark":
+            print("MyServer: I got a POST request")
+
+            content_length = int(self.headers["Content-Length"])
+            print(f"Content data length is {content_length}")
+
+            data = self.rfile.read()
+            list_var_module_level.append(data)
+            self.send_response(201)
 
         else:
             self.send_error(404)
