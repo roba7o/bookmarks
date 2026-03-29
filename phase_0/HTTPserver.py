e423170f0b34d9273279f211945339fc2bb5de39
roba7o <rhm14148@gmail.com>
2026-03-29 09:50:28 +0100

    HTTPServer

--- phase_0/HTTPserver.py
+++ phase_0/HTTPserver.py
@@ -5,10 +5,20 @@ addr = "localhost", 9000
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello There\n")
+        if self.path.lower() == "/about":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"This is a server you have just called!\n")
+
+        elif self.path.lower() == "/robert":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Yep my name!\n")
+
+        else:
+            self.send_error(404)
 
 
 with HTTPServer(addr, MyHandler) as serverr:
