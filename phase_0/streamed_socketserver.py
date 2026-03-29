a35b20af20e5abaa2f3d6f93a26dad919cb0385d
roba7o <rhm14148@gmail.com>
2026-03-29 08:35:26 +0100

    tidy up

--- phase_0/streamed_socketserver.py
+++ phase_0/streamed_socketserver.py
@@ -3,7 +3,8 @@ import socketserver
 
 class echoUpperHandler(socketserver.StreamRequestHandler):
     def handle(self):
-        data = self.rfile.readline()
+        data = self.rfile.readline().rstrip()
+        data += "\n\n<html><body>Hello World</body></html>\r\n\r\n".encode()
         self.wfile.write(data.upper())
 
 
