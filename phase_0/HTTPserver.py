df6231171631a95d561d891eb3b5b60f4aa830d7
roba7o <rhm14148@gmail.com>
2026-03-29 10:07:39 +0100

    serving html with path resolution

--- phase_0/HTTPserver.py
+++ phase_0/HTTPserver.py
@@ -1,5 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from pathlib import Path
 
+local_path = Path(__file__).parent
 addr = "localhost", 9000
 
 
@@ -16,6 +18,13 @@ class MyHandler(BaseHTTPRequestHandler):
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Yep my name!\n")
+
+        elif self.path.lower() == "/document":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            with open(local_path / "phase_0.html", "r") as file:
+                self.wfile.write(file.read().encode())
 
         else:
             self.send_error(404)
