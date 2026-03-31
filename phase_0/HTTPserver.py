14a1e38ccaf6b63da436f0f738c3efdca7a519a1
roba7o <rhm14148@gmail.com>
2026-03-31 08:02:32 +0100

    clean up and attempt at post

--- phase_0/HTTPserver.py
+++ phase_0/HTTPserver.py
@@ -1,41 +1,27 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from pathlib import Path
 
 local_path = Path(__file__).parent
 addr = "localhost", 9000
 
-list_var_module_level = []
+list_var_module_level = [{"book": "Designing Data Intensive Applications", "Page": 88}]
 
 # TODO -> handle requests better with json.dumps and jsonloads
 
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path.lower() == "/about":
-            self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"This is a server you have just called!\n")
-
-        elif self.path.lower() == "/robert":
-            self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"Yep my name!\n")
-
-        elif self.path.lower() == "/document":
-            self.send_response(200)
-            self.send_header("Content-Type", "text/html")
-            self.end_headers()
-            with open(local_path / "phase_0.html", "r") as file:
-                self.wfile.write(file.read(5096).encode())
-
-        elif self.path.lower() == "/bookmark":
+        if self.path.lower() == "/bookmark":
             print("MyServer: I got a GET request")
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
-            self.wfile.write(list_var_module_level.encode())
+            data_dumped = json.dumps(list_var_module_level)
+            self.wfile.write(data_dumped.encode())
+
+        else:
+            self.send_error(404)
 
     def do_POST(self):
         if self.path.lower() == "/bookmark":
@@ -45,7 +31,8 @@ class MyHandler(BaseHTTPRequestHandler):
             print(f"Content data length is {content_length}")
 
             data = self.rfile.read()
-            list_var_module_level.append(data)
+            data_loaded = json.loads(data)
+            list_var_module_level.append(data_loaded)
             self.send_response(201)
 
         else:
