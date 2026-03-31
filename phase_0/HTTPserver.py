df159790964f834ef48ebbea8873cb6a6e662b74
roba7o <rhm14148@gmail.com>
2026-03-31 22:01:19 +0100

    basic get

--- phase_0/HTTPserver.py
+++ phase_0/HTTPserver.py
@@ -5,38 +5,34 @@ from pathlib import Path
 local_path = Path(__file__).parent
 addr = "localhost", 9000
 
-list_var_module_level = [{"book": "Designing Data Intensive Applications", "Page": 88}]
 
 # TODO -> handle requests better with json.dumps and jsonloads
+# plan: post, get, put, delete -> fixed list of vars with indents, 1,2 with starter at 3
+# first just going to get and post working without numbers
+
+bookmark_dict = {
+    1: {"book": "Designing Data Intensive Applications", "Page": 88},
+    2: {"book": "Fluent Python", "Page": 444},
+    3: {"book": "Version Control with Git", "Page": "144"},
+}
 
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path.lower() == "/bookmark":
-            print("MyServer: I got a GET request")
+        if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-Type", "application/json")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            data_dumped = json.dumps(list_var_module_level)
-            self.wfile.write(data_dumped.encode())
+            self.wfile.write(b"Welcome to the api!")
+
+        elif self.path == "/bookmark/":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(bookmark_dict).encode("utf-8"))
 
         else:
-            self.send_error(404)
-
-    def do_POST(self):
-        if self.path.lower() == "/bookmark":
-            print("MyServer: I got a POST request")
-
-            content_length = int(self.headers["Content-Length"])
-            print(f"Content data length is {content_length}")
-
-            data = self.rfile.read()
-            data_loaded = json.loads(data)
-            list_var_module_level.append(data_loaded)
-            self.send_response(201)
-
-        else:
-            self.send_error(404)
+            self.send_error(404, "Not Found!")
 
 
 with HTTPServer(addr, MyHandler) as serverr:
