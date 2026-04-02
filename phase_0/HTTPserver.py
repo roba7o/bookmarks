e44430a7abe21d6b2ac2ca12c64f1414d5c5e3f1
roba7o <rhm14148@gmail.com>
2026-04-02 12:10:48 +0100

    still to do routing

--- phase_0/HTTPserver.py
+++ phase_0/HTTPserver.py
@@ -5,10 +5,7 @@ from pathlib import Path
 local_path = Path(__file__).parent
 addr = "localhost", 9000
 
-
-# TODO -> handle requests better with json.dumps and jsonloads
-# plan: post, get, put, delete -> fixed list of vars with indents, 1,2 with starter at 3
-# first just going to get and post working without numbers
+# ======= Memory Variables =======
 
 bookmark_dict = {
     1: {"book": "Designing Data Intensive Applications", "Page": 88},
@@ -16,42 +13,48 @@ bookmark_dict = {
     3: {"book": "Version Control with Git", "Page": "144"},
 }
 
+_SEQ = 4
+
 
 class MyHandler(BaseHTTPRequestHandler):
-    number_in_memory = 4
+    global _SEQ
 
     def do_GET(self):
         if self.path == "/":
+            basic_200_body = "Welcome to the api!"
             self.send_response(200)
             self.send_header("Content-type", "application/json")
+            self.send_header("Content-Length", int(len(basic_200_body)))
             self.end_headers()
-            self.wfile.write(b"Welcome to the api!")
+            self.wfile.write(basic_200_body.encode("utf-8"))
 
         elif self.path == "/bookmark/":
+            body = json.dumps(bookmark_dict)
             self.send_response(200)
             self.send_header("Content-type", "application/json")
+            self.send_header("Content-length", int(len(body)))
             self.end_headers()
-            self.wfile.write(json.dumps(bookmark_dict).encode("utf-8"))
+            self.wfile.write(body.encode("utf-8"))
 
         else:
             self.send_error(404, "Not Found!")
 
-    def do_POST(self):
-        if self.path == "/bookmark/":
-            content_length = int(self.headers.get("Content-length", 0))
-            body = self.rfile.read(content_length)
+    # def do_POST(self):
+    #     if self.path == "/bookmark/":
+    #         content_length = int(self.headers.get("Content-length", 0))
+    #         body = self.rfile.read(content_length)
 
-            try:
-                data = json.loads(body)
-                # normally would process the data
-                print(data)
-                bookmark_dict[MyHandler.number_in_memory] = data
-                MyHandler.number_in_memory += 1
-                self.send_response(201, "It uploaded -> try a get")
-            except json.JSONDecodeError:
-                self.send_error(400, "not allowed mate")
-        else:
-            self.send_error(404, "Not Found mate!")
+    #         try:
+    #             data = json.loads(body)
+    #             # normally would process the data
+    #             print(data)
+    #             bookmark_dict[MyHandler.number_in_memory] = data
+    #             MyHandler.number_in_memory += 1
+    #             self.send_response(201, "It uploaded -> try a get")
+    #         except json.JSONDecodeError:
+    #             self.send_error(400, "not allowed mate")
+    #     else:
+    #         self.send_error(404, "Not Found mate!")
 
 
 with HTTPServer(addr, MyHandler) as serverr:
