3b0f84a77b313a3865b53e39b76c65edc9f8d2cf
roba7o <rhm14148@gmail.com>
2026-04-12 09:08:50 +0100

    put attempt

--- phase_0/HTTPserver.py
+++ phase_0/HTTPserver.py
@@ -17,8 +17,6 @@ _SEQ = 4
 
 
 class MyHandler(BaseHTTPRequestHandler):
-    global _SEQ
-
     def do_GET(self):
         if self.path == "/":
             basic_200_body = "Welcome to the api! You are at the root page"
@@ -48,22 +46,95 @@ class MyHandler(BaseHTTPRequestHandler):
             except ValueError as E:
                 print(f"Value Error: {E}")
 
-    # def do_POST(self):
-    #     if self.path == "/bookmark/":
-    #         content_length = int(self.headers.get("Content-length", 0))
-    #         body = self.rfile.read(content_length)
+    def do_PUT(self):
+        """
+        Post = new records. Only accepts /bookmark/ without number as uses seq to add.
+        PUT will use sttarts with! -> and changes an existing key.
+        """
+        if self.path == "/":
+            basic_200_body = """
+            Welcome to the api! You can't do anything here with your
+            request. You will need to add /bookmark/ to your endpoint.
+            """
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.send_header("Content-Length", int(len(basic_200_body)))
+            self.end_headers()
+            self.wfile.write(basic_200_body.encode("utf-8"))
 
+        elif self.path.startswith("/bookmark/"):
+            route_number_split = self.path.split("/")
+            try:
+                dict_index = int(route_number_split[2])
+                if dict_index in bookmark_dict.keys():
+                    try:
+                        # Need to error check the the post content
+                        # then insert it into the sequence
+                        # Will attempt it first with clean curl command
+                        length = int(self.headers.get("Content-Length"))
+                        raw = self.rfile.read(length)
+                        try:
+                            raw_loaded = json.loads(raw)
+                            # adding to my list
+                            bookmark_dict[dict_index] = raw_loaded
+                            self.send_response(200)
+
+                            self.send_header("Content-type", "application/json")
+                            basic_put_body = f"Completed PUT! \
+                            The following was replaced at {dict_index}: "
+
+                            full_put_body = basic_put_body + str(raw_loaded)
+                            self.send_header("Content-Length", int(len(full_put_body)))
+                            self.end_headers()
+                            self.wfile.write(full_put_body.encode("utf-8"))
+
+                        except json.JSONDecodeError as json_e:
+                            print(f"JSONDecodeError: {json_e}")
+                    except ValueError:
+                        return None
+                else:
+                    self.send_error(400, "Your index does not exist in the endpoint")
+            except ValueError as E:
+                print(f"Value Error: {E}")
+
+    # def do_PUSH(self):
+    #     """
+    #     Existing Records!!
+    #     """
+    #     if self.path == "/":
+    #         basic_200_body = """
+    #         Welcome to the api! You are at the root page,
+    #         you cannot post anything from here
+    #         """
+    #         self.send_response(200)
+    #         self.send_header("Content-type", "application/json")
+    #         self.send_header("Content-Length", int(len(basic_200_body)))
+    #         self.end_headers()
+    #         self.wfile.write(basic_200_body.encode("utf-8"))
+
+    #     elif self.path.startswith("/bookmark/"):
+    #         route_number_split = self.path.split("/")
     #         try:
-    #             data = json.loads(body)
-    #             # normally would process the data
-    #             print(data)
-    #             bookmark_dict[MyHandler.number_in_memory] = data
-    #             MyHandler.number_in_memory += 1
-    #             self.send_response(201, "It uploaded -> try a get")
-    #         except json.JSONDecodeError:
-    #             self.send_error(400, "not allowed mate")
-    #     else:
-    #         self.send_error(404, "Not Found mate!")
+    #             dict_index = int(route_number_split[2])
+    #             if dict_index in bookmark_dict.keys():
+    #                 try:
+    #                     # Need to error check the the post content
+    #                     # then insert it into the sequence
+    #                     # Will attempt it first with clean curl command
+    #                     length = int(self.headers.get("Content-Length"))
+    #                     raw = self.rfile.read(length)
+    #                     try:
+    #                         raw_loaded = json.loads(raw)
+    #                         # adding to my list
+    #                         bookmark_dict[_SEQ] = raw_loaded
+    #                         _SEQ = _SEQ + 1
+
+    #                     except json.JSONDecodeError as json_e:
+    #                         print(f"JSONDecodeError: {json_e}")
+    #                 except ValueError:
+    #                     return None
+    #         except ValueError as E:
+    #             print(f"Value Error: {E}")
 
 
 with HTTPServer(addr, MyHandler) as serverr:
