6f0ad4ec590c3fe51490b4e2c71b7a205c3ea752
roba7o <rhm14148@gmail.com>
2026-04-12 10:13:13 +0100

    post working

--- phase_0/HTTPserver.py
+++ phase_0/HTTPserver.py
@@ -5,6 +5,13 @@ from pathlib import Path
 local_path = Path(__file__).parent
 addr = "localhost", 9000
 
+
+"""
+Example Curl Command:
+curl -v -X PUT localhost:9000/bookmark/1 -d '{"book": "desisiningggg", "Page": 555}'
+
+"""
+
 # ======= Memory Variables =======
 
 bookmark_dict = {
@@ -95,6 +102,47 @@ class MyHandler(BaseHTTPRequestHandler):
                     self.send_error(404, "Resource not found in the endpoint")
             except ValueError as E:
                 print(f"Value Error: {E}")
+
+    def do_POST(self):
+        """
+        New entries! Need to use the global sequencer!
+        """
+        if self.path == "/":
+            basic_200_body = """
+            Welcome to the api! You can't do anything here with your
+            request. You will need to add /bookmark/new_book to your endpoint.
+            """
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.send_header("Content-Length", int(len(basic_200_body)))
+            self.end_headers()
+            self.wfile.write(basic_200_body.encode("utf-8"))
+
+        elif self.path == ("/bookmark/new_book"):
+            try:
+                length = int(self.headers.get("Content-Length"))
+                raw = self.rfile.read(length)
+                try:
+                    raw_loaded = json.loads(raw)
+                    # adding to my list
+                    global _SEQ
+                    _SEQ = _SEQ + 1
+                    bookmark_dict[_SEQ] = raw_loaded
+                    self.send_response(201)
+
+                    self.send_header("Content-type", "application/txt")
+                    basic_put_body = f"Completed POST! \
+                    The following was added at {_SEQ}: "
+
+                    full_put_body = basic_put_body + str(raw_loaded)
+                    self.send_header("Content-Length", int(len(full_put_body)))
+                    self.end_headers()
+                    self.wfile.write(full_put_body.encode("utf-8"))
+
+                except json.JSONDecodeError as json_e:
+                    self.send_error(400, f"JSONDecodeError: {json_e}")
+            except ValueError as val_e:
+                self.send_error(400, f"value error: {val_e}")
 
 
 with HTTPServer(addr, MyHandler) as serverr:
