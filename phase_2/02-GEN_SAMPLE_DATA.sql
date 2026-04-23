ad2d0e138ee0d027ae1fc2ef991b78593aeb4fb5
roba7o <rhm14148@gmail.com>
2026-04-23 09:06:24 +0100

    init sql files

---
+++ phase_2/02-GEN_SAMPLE_DATA.sql
@@ -0,0 +1,5 @@
+INSERT INTO BOOKMARKS (TITLE, AUTHOR, PAGE) VALUES
+    ('100 Years of Solitude', 'Gabriel Garcia Marquez', 201),
+    ('Berlin the Downfall', 'Anthony Beevor', 400),
+    ('Damascus Station', 'David Mclosckey', 10);
+
---
+++ phase_2/main.py
@@ -0,0 +1,58 @@
+import os
+
+import asyncpg
+from dotenv import load_dotenv
+from starlette.applications import Starlette
+from starlette.endpoints import HTTPEndpoint
+from starlette.exceptions import HTTPException
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+load_dotenv()
+
+"""
+From the docs but not a decorator approach
+
+jsut run main -> its not working
+
+TODO: but the standard pattern is a connection pool (asyncpg.create_pool())
+created once at app startup and shared across requests.
+Starlette has lifespan hooks for this
+"""
+
+
+class BookMarkItem(HTTPEndpoint):
+    async def get(self, request):
+        conn = await asyncpg.connect(
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            host="127.0.0.1",
+            port=5432,
+        )
+        try:
+            book_index = request.path_params["book_index"]
+
+            bookmark_item = await conn.fetchrow(
+                "SELECT * FROM public.bookmarks WHERE id = $1", book_index
+            )
+            print(
+                f"bookmark_item is type: {type(bookmark_item)} \
+                  and is value: {bookmark_item}"
+            )
+
+            if bookmark_item is None:
+                raise HTTPException(404)
+
+            return JSONResponse(dict(bookmark_item))
+
+        finally:
+            await conn.close()
+
+
+app = Starlette(
+    debug=True,
+    routes=[
+        Route("/bookmarks/{book_index:int}", endpoint=BookMarkItem),
+    ],
+)
