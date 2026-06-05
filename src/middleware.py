960ca2c7be3c2df6363b72a0e29d9a40050b7fa3
roba7o <rhm14148@gmail.com>
2026-06-05 09:19:29 +0100

    middleware attempt

---
+++ src/middleware.py
@@ -0,0 +1,32 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+from auth import decode_token
+
+
+class AuthenticationMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        # tryin for get now
+        if request.method in ["OPTIONS", "GET"]:
+            return await call_next(request)
+
+        if "auth" in request.url.path:
+            return await call_next(request)
+
+        bearer_data = request.headers.get("Authorization")
+        token = bearer_data.replace("Bearer ", "") if bearer_data else None
+
+        if not token:
+            return JSONResponse(
+                content="Missing auth token mate",
+                status_code=401,
+            )
+
+        if not decode_token(token):
+            return JSONResponse(
+                content="INVALID auth token mate",
+                status_code=401,
+            )
+
+        return await call_next(request)
