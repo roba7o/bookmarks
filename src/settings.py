4c293e07d05c48fc024fc1ebbf8719d477130567
roba7o <rhm14148@gmail.com>
2026-06-06 09:10:09 +0100

    wip

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,8 @@ import os
 
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # configuring logging globally.
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -15,5 +17,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_USER = os.getenv("DB_USER")
 DB_NAME = os.getenv("DB_NAME")
 
+# tokens env
 
-load_dotenv()
+JWT_SECRET = os.environ["JWT_SECRET"]
+JWT_ALGO = os.environ["JWT_ALGORITHM"]
