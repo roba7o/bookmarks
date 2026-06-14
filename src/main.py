a71c29416c5075217a13b63cde1c91526ef6e148
roba7o <rhm14148@gmail.com>
2026-06-14 21:20:42 +0100

    git working

--- src/main.py
+++ src/main.py
@@ -23,20 +23,14 @@ from src.exception_handlers import (
 )
 from src.middleware import AuthenticationMiddleware
 from src.schemas import metadata
-from src.settings import DB_NAME, DB_PASSWORD, DB_USER, logger
+from src.settings import DATABASE_URL, DB_NAME, logger
 
 
 @asynccontextmanager
 async def lifespan(app) -> AsyncGenerator:
-    password = DB_PASSWORD
-    user = DB_USER
-    db_name = DB_NAME
+    app.state.engine = create_async_engine(f"{DATABASE_URL}", echo=False)
 
-    app.state.engine = create_async_engine(
-        f"postgresql+asyncpg://{user}:{password}@localhost:5432/{db_name}", echo=False
-    )
-
-    logger.info(f"Engine booted for db:'{db_name}' via lifespan")
+    logger.info(f"Engine booted for db:'{DB_NAME}' via lifespan")
 
     async with app.state.engine.begin() as conn:
         await conn.run_sync(metadata.create_all)
