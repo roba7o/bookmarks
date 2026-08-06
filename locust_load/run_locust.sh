d00660cec529daac9d806f7cce5196706e07514d
roba7o <rhm14148@gmail.com>
2026-08-06 15:23:37 +0100

    removing headlessness

--- locust_load/run_locust.sh
+++ locust_load/run_locust.sh
@@ -5,6 +5,6 @@ outdir="locust_load/results/$1/$ts"         # results/basic/06082026-1442/
 mkdir -p "$outdir"                          # create it (+ parents)
 
 uv run locust -f "locust_load/${1}_locust.py" \
-    --headless -u 100 -r 10 -t 60s \
+    -u 100 -r 10 -t 60s \
     --csv="$outdir/$1" \
     --html="$outdir/$1.html"
