9cd9c71e92391ea5997a95dff8e6d606433f461e
roba7o <rhm14148@gmail.com>
2026-08-06 14:34:31 +0100

    shell command running

--- locust_load/run_locust.sh
+++ locust_load/run_locust.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # usage: run locust test files with a name and will save results
-# ./locust_load/run_locust.sh <testfile> <name>
-uv run locust -f "locust_load/$1" \
+# ./locust_load/run_locust.sh <testfile> (without.py)
+name="${1}_locust.py"
+ECHO "Running: $name"
+
+
+uv run locust -f "locust_load/$name" \
     --headless -u 100 -r 10 -t 60s \
-    --csv="locust_load/results/$1$2" \
-    --html="locust_load/results/$1/$2.html"
+    --csv="locust_load/results/$1/$1" \
+    --html="locust_load/results/$1/$1.html"
 
 
